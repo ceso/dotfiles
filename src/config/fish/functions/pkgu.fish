07520c7d96617853b25b51312fd331bb0db1d512
Pablo Castellazzi <pablo.castellazzi@icloud.com>
2025-11-10 09:41:39 -0300

    remove unused scripts

--- src/config/fish/functions/pkgu.fish
+++
@@ -1,9 +0,0 @@
-function pkgu --description 'uninstall .pkg packages' --argument package
-    echo "Uninstalling $package"
-    pkgutil --files $package | while read _file
-        if ! pkgutil --file-info $_file | grep "^pkgid: " | grep -v $package >/dev/null
-            rm -rf $file || echo >&2 "E: can not remove $file"
-        end
-    end
-    pkgutil --forget $package
-end
